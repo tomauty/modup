2045afd89f3b81f1418e808872f91cb0da7b8a55
obble <mkentnotpaul@gmail.com>
2015-10-05 14:49:49 +0100

    .5
    initial incomplete commit for bugsharing. THIS DOES NOT WORK YET

---
+++ list.lua
@@ -0,0 +1,18 @@
+
+
+    -- here you can add, take-away and style spells you want to track.
+    -- this can easily be added to
+    -- including skills beyond those procs and buffs already handled by SpellActivationOverlay.
+    -- it will NOT currently handle cooldowns.
+
+    modspells = {
+       {    -- PRIEST                               -- KEY
+            id = 17,                                -- spellid
+            priority = 2,                           -- controls positioning of the string. lower number means it will stack above those w/ a higher one.
+            show = true,                            -- toggle whether this aura will be tracked.
+            Name = 'Power Word: Shield',            -- aura name.
+            symbol = '( )',                         -- symbol or text you want to represent the aura with.
+            size = 24,                              -- size of text.
+            colour = { 250/255, 239/255, 144/255 }, -- RGB colour of text, can be numeric (0-1) or out of 255. in this case it's a nice yellow.
+        },
+    }
